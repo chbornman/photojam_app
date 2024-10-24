4924ffe02b0c417c99105b62beedc0a47042510c
chbornman <101027896+chbornman@users.noreply.github.com>
2024-10-24 10:55:17 -0400

    Revert "attempt to add login page"
    This reverts commit 89e0f9bbb031020f154a0f29402a767d65823501.

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,17 @@
-import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 import 'home_page.dart';  // Import home page
 import 'journey_page.dart';  // Import journey page
 import 'profile_page.dart';  // Import profile page
 import 'signup_page.dart';         // Import the Sign Up Page
 import 'journeys/the_art_of_storytelling.dart'; // Import the Current Journey Page
-import 'package:appwrite/models.dart' as models;
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Client client = Client()
-      .setEndpoint("http://localhost/v1")
-      .setProject("67180af036f8f");
-  Account account = Account(client);
-
-  runApp(MaterialApp(
-    home: MyApp(account: account),
-  ));
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  final Account account;
-
-  MyApp({required this.account});
-
-  @override
-  MyAppState createState() {
-    return MyAppState();
-  }
-  
-  
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Photo Jam',
       theme: ThemeData(
@@ -43,94 +22,6 @@ class MyApp extends StatefulWidget {
     );
   }
 }
-
-class MyAppState extends State<MyApp> {
-  models.User? loggedInUser;
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController nameController = TextEditingController();
-
-  Future<void> login(String email, String password) async {
-    await widget.account.createEmailPasswordSession(email: email, password: password);
-    final user = await widget.account.get();
-    setState(() {
-      loggedInUser = user;
-    });
-  }
-
-  Future<void> register(String email, String password, String name) async {
-    await widget.account.create(
-        userId: ID.unique(), email: email, password: password, name: name);
-    await login(email, password);
-  }
-
-  Future<void> logout() async {
-    await widget.account.deleteSession(sessionId: 'current');
-    setState(() {
-      loggedInUser = null;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(loggedInUser != null
-                ? 'Logged in as ${loggedInUser!.name}'
-                : 'Not logged in'),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(labelText: 'Name'),
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    login(emailController.text, passwordController.text);
-                  },
-                  child: Text('Login'),
-                ),
-                SizedBox(width: 16.0),
-                ElevatedButton(
-                  onPressed: () {
-                    register(emailController.text, passwordController.text,
-                        nameController.text);
-                  },
-                  child: Text('Register'),
-                ),
-                SizedBox(width: 16.0),
-                ElevatedButton(
-                  onPressed: () {
-                    logout();
-                  },
-                  child: Text('Logout'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 
 class MainPage extends StatefulWidget {
   @override
